58f0190cca98837697789164b1f303a349421fa8
AloyFusion <48078029+AloyFusion@users.noreply.github.com>
2022-02-10 14:17:37 -0600

    pain

--- src/src/KeyListener.java
+++ src/src/KeyListener.java
@@ -17,13 +17,15 @@ public class KeyListener extends KeyAdapter {
         snake.vy = 0;
 
         if (e.getKeyChar() == 'w')
-            snake.vx = 1;
+            snake.vy = -1;
         if (e.getKeyChar() == 'a')
             snake.vx = -1;
         if (e.getKeyChar() == 's')
-            snake.vy = -1;
+            snake.vy = 1;
         if (e.getKeyChar() == 'd')
             snake.vx = 1;
+
+        System.out.println(snake.vx + " " + snake.vy);
     }
 
     @Override
--- src/src/Snake.java
+++ src/src/Snake.java
@@ -18,10 +18,10 @@ public class Snake extends JComponent implements Runnable {
         jFrame.addKeyListener(listener);
 
         points = new ArrayList<Point>(Arrays.asList(
-                new Point(HEADSTARTINGPOSX, HEADSTARTINGPOSY),
                 new Point(HEADSTARTINGPOSX + 1, HEADSTARTINGPOSY),
                 new Point(HEADSTARTINGPOSX + 2, HEADSTARTINGPOSY),
-                new Point(HEADSTARTINGPOSX + 3, HEADSTARTINGPOSY)
+                new Point(HEADSTARTINGPOSX + 3, HEADSTARTINGPOSY),
+                new Point(HEADSTARTINGPOSX + 4, HEADSTARTINGPOSY)
         ));
 
         length = points.size() - 1;
@@ -35,10 +35,12 @@ public class Snake extends JComponent implements Runnable {
 
         for (int i = 0 ; i < points.size() ; i++) {
             draw.setColor(Color.red);
-            //if (i != 0)
-                //draw.setColor(Color.blue);
+            if (i != 0)
+                draw.setColor(Color.blue);
             draw.fillRect(convert(points.get(i).x, "x"), convert(points.get(i).y, "y"), Game.xScale, Game.yScale);
         }
+        draw.setColor(Color.yellow);
+        draw.fillRect(convert(points.get(0).x, "x"), convert(points.get(0).y, "y"), Game.xScale, Game.yScale);
     }
 
     @Override
@@ -62,22 +64,34 @@ public class Snake extends JComponent implements Runnable {
     private void shift() {
         System.out.println(points);
         for (int i = points.size() - 1 ; i >= 0 ; i--) {
+            if (i == 0) {
+                points.set(i, nextPoint);
+                System.out.println("set " + i + " to nextPoint: " + nextPoint);
+            }
+
+            else {
+                points.set(i, points.get(i - 1));
+                System.out.println("set " + i + " to " + points.get(i - 1));
+            }
+            /*
             try {
                 points.set(i, points.get(i - 1));
             } catch (IndexOutOfBoundsException e) {
                 points.set(i, nextPoint);
                 System.out.println("exception");
             }
-        }
 
-        if (length < points.size() - 1) {
+             */
+        }
+        //points.set(0, nextPoint);
+
+        //if (length < points.size() - 1) {
             //points.remove(points.size() - 1);
-            Game.convert()
 
-        }
-        else if (length > points.size() - 1) {
+        //}
+        //else if (length > points.size() - 1) {
             //points.add(new Point(-1, -1));
-        }
+        //}
     }
 
     public void nextFrame() {repaint();}
